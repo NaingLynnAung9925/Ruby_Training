1a67a577b552d9f3c7fc0ad25ae85aacca36f883
Naing Lynn Aung <unrivaledking1@gmail.com>
2022-04-25 21:37:57 +0630

    Updating Tuto_02 code

--- Tutorials/Tutorial02/tutorial02.rb
+++ Tutorials/Tutorial02/tutorial02.rb
@@ -1,9 +1,37 @@
-puts "Enter your age"
+require 'date'
 
-age = gets.chomp.to_i
+puts "Enter your birthday year"
+user_birth_year = gets.chomp.to_i
 
-if age >= 19
-  print "(#{ age } , adult)"
+puts "Enter your birthday month"
+user_birth_month = gets.chomp.to_i
+
+puts "Enter your birthday date"
+user_birth_date = gets.chomp.to_i
+
+year = Time.new.year
+month = Time.new.month
+date = Time.new.day
+
+birth_year = year - user_birth_year
+
+birth_month = month - user_birth_month
+
+if birth_month <= -1
+   birthday_month = birth_month + 12
+   birthday_year = birth_year - 1
+else
+  birthday_month = birth_month
+  birthday_year = birth_year
+end
+
+birth_date = date - user_birth_date
+
+age = "#{birthday_year} years , #{birthday_month} months , #{birth_date} days"
+puts "Your age is #{age} and"
+
+if birth_year >= 19
+  print "You are adult"
   else
-    print "(#{ age } , child)"
-  end
+    print "You are not adult"
+end

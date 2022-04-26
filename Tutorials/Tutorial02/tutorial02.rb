606c826feece3d43e34f962f762ed6d199c2ddbe
Naing Lynn Aung <unrivaledking1@gmail.com>
2022-04-26 10:43:26 +0630

    Updating Tuto_02

--- Tutorials/Tutorial02/tutorial02.rb
+++ Tutorials/Tutorial02/tutorial02.rb
@@ -18,20 +18,40 @@ birth_year = year - user_birth_year
 birth_month = month - user_birth_month
 
 if birth_month <= -1
+
    birthday_month = birth_month + 12
    birthday_year = birth_year - 1
+
 else
+
   birthday_month = birth_month
   birthday_year = birth_year
+
 end
 
 birth_date = date - user_birth_date
 
-age = "#{birthday_year} years , #{birthday_month} months , #{birth_date} days"
+if birth_date <= -1 && date < 31
+
+  birthday_date = birth_date + 31
+  user_month = birthday_month - 1
+
+else
+
+  birthday_date = birth_date
+  user_month = birthday_month
+
+end
+
+age = "#{birthday_year} years , #{user_month} months , #{birthday_date} days"
 puts "Your age is #{age} and"
 
 if birth_year >= 19
+
   print "You are adult"
-  else
-    print "You are not adult"
+
+else
+
+  print "You are not adult"
+
 end
